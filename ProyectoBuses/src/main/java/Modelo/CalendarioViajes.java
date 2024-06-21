6e0352aa6ac272d3f330504045ae669174c311b5
AndresChCy <aichaparrom@gmail.com>
2024-06-20 22:37:23 -0400

    Se añade beta para llenar el calendario con viajes aleatorios

--- ProyectoBuses/src/main/java/Modelo/CalendarioViajes.java
+++ ProyectoBuses/src/main/java/Modelo/CalendarioViajes.java
@@ -1,6 +1,8 @@
 package Modelo;
 
 import java.util.ArrayList;
+import java.time.*;
+
 
 /**
  * Clase singleton que representa la base de datos de los viajes en bus
@@ -22,5 +24,30 @@ public class CalendarioViajes {
             fechas = new CalendarioViajes();
         }
         return fechas;
+    }
+    public void añadirViaje(ViajeBus viaje ){
+    }
+    public void quitarViaje(ViajeBus viaje){}
+    //Falta exceptions y completar metodos
+    public void llenarDatos(Ciudades origen,Ciudades destino,LocalDate dia ){
+        int hora;
+        int minutos;
+        LocalTime horario;
+        ViajeBus viaje;
+        Period diff = LocalDate.now().until(dia);
+        int numViajes = (int) (Math.floor(Math.random()*(7)+4)); //Numeros entre 10 y 4
+        for (int i = 0;i<=numViajes;i++ ){
+            hora = (int) (Math.floor(Math.random()*(25)));
+            minutos = 5 * (int) (Math.floor(Math.random()*(12)));
+            horario = LocalTime.of(hora,minutos);
+            viaje = new ViajeBus(LocalDateTime.of(dia,horario),origen,destino );
+            calendario[origen.ordinal()][destino.ordinal()][diff.getDays()].add(viaje);
+        }
+    }
+    public void ordenarViajes(Ciudades origen,Ciudades destino,LocalDate dia){}
+    public void actualizarDia(Ciudades origen,Ciudades destino){
+        int numViajes = calendario[origen.ordinal()][destino.ordinal()][0].size();
+        for (int i = 0;i < numViajes ;i++){
+        }
     }
 }
--- ProyectoBuses/src/main/java/Modelo/ViajeBus.java
+++ ProyectoBuses/src/main/java/Modelo/ViajeBus.java
@@ -1,4 +1,14 @@
 package Modelo;
 
+import java.time.*;
+
 public class ViajeBus {
+    private LocalDateTime fecha;
+    private Ciudades origen;
+    private Ciudades destino;
+    public ViajeBus(LocalDateTime fecha,Ciudades origen, Ciudades destino ){
+        this.fecha = fecha;
+        this.origen = origen;
+        this.destino = destino;
+    }
 }
